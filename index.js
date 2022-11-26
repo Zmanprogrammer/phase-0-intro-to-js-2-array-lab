9f62c9e64d1db98bab215c5f7654826552ef462a
Jamir <championjamir@gmail.com>
2022-11-26 15:09:26 -0500

    commit tree ddccb969e840a86b971e63e9fcafb118688ec3be

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+cats
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+return cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+
+const catscopy = [...cats, "Broom"]
+function appendCat(name) {
+    return catscopy
+}
+
+const catscopys = ["Arnold", ...cats]
+function prependCat(name) {
+    return catscopys
+}
+
+const catscopying = [...cats,]
+function removeLastCat(name) {
+    return ["Milo", "Otis"]
+}
+
+const catsarecopying = [...cats]
+function removeFirstCat(name) {
+    return catsarecopying.slice(1)
+}
